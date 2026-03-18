41b196455cf4f67ccb0f1ce364a2667e795ebf55
Chitrank Yadav <chitrankkumar187@gmail.com>
2026-03-19 00:23:46 +0530

    Refactor server.js for consistency and clarity

--- server.js
+++ server.js
@@ -1,36 +1,40 @@
 const http = require("http");
+constconst http = require("http");
 const WebSocket = require("ws");
 
 const server = http.createServer((req, res) => {
     res.writeHead(200);
-    res.end("Server is running");
+    res.end("Server running");
 });
 
 const wss = new WebSocket.Server({ server });
 
 let clients = [];
 
-wss.on("connection", function connection(ws) {
+wss.on("connection", function (ws) {
     console.log("New player connected");
+
     clients.push(ws);
 
-    ws.on("message", function incoming(message) {
+    ws.on("message", function (message) {
         console.log("Received:", message.toString());
 
-        clients.forEach(client => {
+        // 🔥 SEND TO ALL OTHER PLAYERS
+        clients.forEach(function (client) {
             if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
         });
     });
 
-    ws.on("close", () => {
+    ws.on("close", function () {
         console.log("Player disconnected");
         clients = clients.filter(c => c !== ws);
     });
 });
 
 const PORT = process.env.PORT || 10000;
+
 server.listen(PORT, () => {
-    console.log("Server running on port", PORT);
+    console.log("Server started on port", PORT);
 });
