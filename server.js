c477dd2b419d155434b81b394953b760ec80cbc4
Chitrank Yadav <chitrankkumar187@gmail.com>
2026-03-18 23:30:35 +0530

    Log connection and disconnection events

--- server.js
+++ server.js
@@ -5,18 +5,22 @@ const wss = new WebSocket.Server({ port: process.env.PORT || 10000 });
 let clients = [];
 
 wss.on("connection", function connection(ws) {
+    console.log("New player connected");
     clients.push(ws);
 
     ws.on("message", function incoming(message) {
-        // send message to all players
+        console.log("Received:", message.toString());
+
+        // 🔥 SEND TO ALL OTHER PLAYERS (NOT SELF)
         clients.forEach(client => {
-            if (client.readyState === WebSocket.OPEN) {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
                 client.send(message.toString());
             }
         });
     });
 
     ws.on("close", () => {
+        console.log("Player disconnected");
         clients = clients.filter(c => c !== ws);
     });
 });
